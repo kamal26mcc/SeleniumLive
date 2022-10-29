5b9a3818776d310b7cf27e87cf5a9659a66eb6bf
Kamal Kathirvel <105717377+Kamalkathirvel@users.noreply.github.com>
2022-10-30 04:29:57 +0530

    new update

--- src/main/java/tableconsept/TableAutomationScenario.java
+++ src/main/java/tableconsept/TableAutomationScenario.java
@@ -10,44 +10,42 @@ import common.AssignmentGenerics;
 
 public class TableAutomationScenario extends AssignmentGenerics {
 
-	WebDriver driver = null;
+    WebDriver driver = null;
 
-	public void getAllDataFromTable() {
+    public void getAllDataFromTable() {
 
-		int i = 0;
+        int i = 0;
 
-		WebElement table = driver
-				.findElement(By.xpath("//table[@class='table table-bordered table-hover mx-3']/tbody"));
+        WebElement table = driver
+            .findElement(By.xpath("//table[@class='table table-bordered table-hover mx-3']/tbody"));
 
-		WebElement rows = table.findElement(By.tagName("tr"));
+        WebElement rows = table.findElement(By.tagName("tr"));
 
-		for (WebElement row : rows)
-		{
+        for (WebElement row: rows) {
 
-			i = i + 1;
+            i = i + 1;
 
-			WebElement column = row.findElement(By.tagName("td"));
+            WebElement column = row.findElement(By.tagName("td"));
 
-			for (WebElement cell : cells)
-			{
+            for (WebElement cell: cells) {
 
-				String value = cell.getText();
+                String value = cell.getText();
 
-				System.out.println("Table Data");
-				System.out.println(value + " ||");
+                System.out.println("Table Data");
+                System.out.println(value + " ||");
 
-			}
+            }
 
-		}
+        }
 
-	}
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		TableAutomationScenario obj = new TableAutomationScenario();
-		obj.driver = obj.openBrowser("https://intellect.allthing.co.in/intellect/html_codes/webtable.html", "chrome");
-		obj.getAllDataFromTable();
+        TableAutomationScenario obj = new TableAutomationScenario();
+        obj.driver = obj.openBrowser("https://intellect.allthing.co.in/intellect/html_codes/webtable.html", "chrome");
+        obj.getAllDataFromTable();
 
-	}
+    }
 
 }
